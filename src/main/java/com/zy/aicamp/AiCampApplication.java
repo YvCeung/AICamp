ef1b8a4b09e07d646b4e467be0c088e4e60edb92
YvZhang <zytuatua@163.com>
2025-05-08 23:14:42 +0800

    init:first commit

---
+++ src/main/java/com/zy/aicamp/AiCampApplication.java
@@ -0,0 +1,13 @@
+package com.zy.aicamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCampApplication.class, args);
+    }
+
+}
