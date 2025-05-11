1c871cf954fd9bfc0d60d782b7093304c202049e
YvZhang <zytuatua@163.com>
2025-05-11 15:57:11 +0800

    feature:添加会话记忆

--- src/main/java/com/zy/aicamp/config/AIConfiguration.java
+++ src/main/java/com/zy/aicamp/config/AIConfiguration.java
@@ -1,7 +1,10 @@
 package com.zy.aicamp.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,11 +18,18 @@ import org.springframework.context.annotation.Configuration;
 public class AIConfiguration {
 
     @Bean
-    public ChatClient chatClient(OllamaChatModel model){
+    public ChatMemory chatMemory(){
+        //本质是一个Map把会话id和Message给存了起来
+        return new InMemoryChatMemory();
+    }
+
+    @Bean
+    public ChatClient chatClient(OllamaChatModel model, ChatMemory chatMemory){
         return ChatClient.builder(model)
                 .defaultSystem("你是一个严厉的小学数学老师，叫孙晓晓，请以数学老师的身份和语气回答问题")
                 //用来专门记录日志的一个环绕增强
-                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .defaultAdvisors(new SimpleLoggerAdvisor(),
+                        new MessageChatMemoryAdvisor(chatMemory))
                 .build();
     }
 }
--- src/main/java/com/zy/aicamp/controller/ChatController.java
+++ src/main/java/com/zy/aicamp/controller/ChatController.java
@@ -1,7 +1,9 @@
 package com.zy.aicamp.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
@@ -33,8 +35,10 @@ public class ChatController {
      * @return
      */
     @RequestMapping(value = "/chatstream",produces = "text/html;charset=utf-8")
-    public Flux<String> chatstream(String prompt){
+    public Flux<String> chatstream(String prompt, @RequestParam(name = "chatId")String chatId){
         return chatClient.prompt()
+                //存储会话id
+                .advisors(advisorSpec -> advisorSpec.param(AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY,chatId))
                 .user(prompt)
                 .stream().content();
     }
