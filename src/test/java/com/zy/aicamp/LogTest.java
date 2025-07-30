797caa560294bb8097b59d229197abbd82ec1901
xiaoyu <zytuatua@163.com>
2025-07-30 22:18:14 +0800

    optimize:调整项目结构去除无用依赖

---
+++ src/test/java/com/zy/aicamp/LogTest.java
@@ -0,0 +1,17 @@
+package com.zy.aicamp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Description
+ * @Author zy
+ * @Date 2025/6/21 16:39
+ **/
+@Slf4j
+public class LogTest {
+    @Test
+    void printTest(){
+        log.info("test");
+    }
+}
